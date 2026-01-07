ffc5c3ab2c3e5d342c7d5153ba02ec7841994eac
Alina <kybrac.alina1983@gmail.com>
2026-01-07 12:53:29 +0200

    Commit

---
+++ main.py
@@ -0,0 +1,58 @@
+first_name = 'Alina '
+last_name = 'K'
+full_name = first_name + last_name
+country = 'Ukraine'
+city = 'Kiev'
+age, year = 18, 2026
+is_married = bool
+is_true = False
+is_light_on = True
+
+# Printing out types
+print(type(first_name))
+print(type(last_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print('First name length: ', len(first_name))
+print('Last name length: ', len(last_name))
+
+
+
+num_one = 5
+num_two = 4
+variable_total = num_one + num_two
+variable_diff = num_two - num_one
+variable_product = num_two * num_one
+variable_division = num_one / num_two
+variable_remainder = num_two % num_one
+variable_exp = num_two ** num_one
+floor_division = num_one // num_two
+print(variable_total, variable_diff, variable_product, variable_exp,variable_division, variable_remainder, floor_division)
+
+circle_radius = 30
+area_of_circle = 3.14 * (circle_radius ** 2)
+circum_of_circle = 90 * 3.14
+print(area_of_circle)
+
+
+radius = input('Circle radius: ')
+area = 3.14 * (int(radius) **2)
+print(area)
+
+
+user_fname = input('What is your first name? ')
+user_lname = input('What is your last name? ')
+user_country = input('Where you from? ')
+user_age = input('How old are you? ')
+print(user_country, user_age,user_lname, user_fname)
+
+help('keywords')
+
+
+
